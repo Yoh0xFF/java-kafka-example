b3be55319b8ea50d1f619380bd765eb04237764e
Ioram Gordadze <Yoh0xFF@gmail.com>
2019-09-17 17:11:07 +0400

    Create idempotent kafka producer.

--- src/main/java/io/example/twitter/TwitterProducer.java
+++ src/main/java/io/example/twitter/TwitterProducer.java
@@ -99,6 +99,7 @@ public class TwitterProducer {
 
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
